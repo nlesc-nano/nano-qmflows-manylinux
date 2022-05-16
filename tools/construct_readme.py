94035f5de4dd0e9d04a5129f81f8182d2fe3dcee
Bas van Beek <b.f.van.beek@vu.nl>
2022-05-16 12:31:59 +0200

    MAINT: Explicitly past `--host` when building MacOS dependencies

--- tools/construct_readme.py
+++ tools/construct_readme.py
@@ -5,7 +5,7 @@ import argparse
 import datetime
 
 
-def _parse_env_vars() -> "tuple[dict[str, str], str]":
+def _parse_env_vars() -> "dict[str, str]":
     names = [
         "HIGHFIVE_VERSION",
         "BOOST_VERSION",
@@ -15,23 +15,17 @@ def _parse_env_vars() -> "tuple[dict[str, str], str]":
     ]
     i = len("_VERSION")
     try:
-        version_dict = {k[:-i]: os.environ[k] for k in names}
-        deployment_target = os.environ["MACOSX_DEPLOYMENT_TARGET"]
+        return {k[:-i]: os.environ[k] for k in names}
     except KeyError as ex:
-        raise EnvironmentError(f"Missing environment variable: {ex}") from None
-    else:
-        return version_dict, deployment_target
+        raise ValueError(f"Missing environment variable: {ex}") from None
 
 
-VERSION_DICT, MACOSX_DEPLOYMENT_TARGET = _parse_env_vars()
+VERSION_DICT = _parse_env_vars()
 
 
 def main(output: "str | os.PathLike[str]") -> None:
     with open(output, "w", encoding="utf8") as f:
-        f.write(
-            f"Auto-generated MacOS {MACOSX_DEPLOYMENT_TARGET} C++ build "
-            f"dependencies ({datetime.datetime.now()})\n\n"
-        )
+        f.write(f"Auto-generated MacOS C++ build dependencies ({datetime.datetime.now()})\n\n")
         f.write("Packages\n--------\n")
         for k, v in VERSION_DICT.items():
             f.write(f"- {k}: {v}\n")
@@ -41,8 +35,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         usage="python construct_readme.py README.rst", description=__doc__
     )
-    parser.add_argument(
-        "path", help="The path to the to-be created README file"
-    )
+    parser.add_argument("path", help="The path to the to-be created README file")
     args = parser.parse_args()
     main(args.path)
