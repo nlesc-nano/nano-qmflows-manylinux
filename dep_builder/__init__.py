2f7780e820f33b9b004c339c93809ffc50bf1094
Bas van Beek <b.f.van.beek@vu.nl>
2022-06-27 11:22:35 +0200

    DOC: Expand the docstrings of the dependency builder python API

--- dep_builder/__init__.py
+++ dep_builder/__init__.py
@@ -1,4 +1,32 @@
-"""Utilities for building (and logging) packages."""
+"""Utilities for building (and logging) packages.
+
+Index
+-----
+.. currentmodule:: dep_builder
+.. autosummary::
+    download_and_unpack
+    configure
+    read_config_log
+    build
+    parse_version
+    BaseTimeLogger
+    TimeLogger
+    logger
+    __version__
+
+API
+---
+.. autofunction:: download_and_unpack
+.. autofunction:: configure
+.. autofunction:: read_config_log
+.. autofunction:: build
+.. autofunction:: parse_version
+.. autoclass:: BaseTimeLogger
+.. autoclass:: TimeLogger
+.. autodata:: logger
+.. autodata:: __version__
+
+"""
 
 from ._version import __version__
 from ._logger import logger, TimeLogger, BaseTimeLogger
--- dep_builder/_core.py
+++ dep_builder/_core.py
@@ -19,7 +19,19 @@ __all__ = ["download_and_unpack", "configure", "read_config_log", "build", "pars
 
 
 def parse_version(version: str) -> Version:
-    """Check that a PEP 440-compliant version is provided."""
+    """Check that a PEP 440-compliant version is provided.
+
+    Parameters
+    ----------
+    version : str
+        The to-be validated version.
+
+    Returns
+    -------
+    packaging.version.Version
+        The fully parsed version object.
+
+    """
     ret = Version(version)
     logger.info(f"Successfully parsed {version!r}")
     return ret
@@ -30,7 +42,24 @@ def download_and_unpack(
     archive_path: str | os.PathLike[str] = "tmp.tar.gz",
     delete_archive: bool = True,
 ) -> Path:
-    """Download and unpack the archive from the provided url."""
+    """Download and unpack the archive from the provided URL.
+
+    Parameters
+    ----------
+    url : str
+        The URL to the to-be downloaded tar archive.
+    archive_path : str | os.PathLike[str]
+        The (absolute) path to the to-be downloaded archive.
+    delete_archive : bool
+        Whether the archive should be deleted after the download is complete.
+
+    Returns
+    -------
+    pathlib.Path
+        The absolute path to the downloaded and extracted archive.
+
+    """
+    archive_path = os.fsdecode(archive_path)
     logger.info(f"Download {url!r}")
     try:
         with open(archive_path, "wb") as f1, requests.get(url, allow_redirects=True) as r:
@@ -41,11 +70,10 @@ def download_and_unpack(
             root = {i.split(os.sep)[0 if not i.startswith(".") else 1] for i in f2.getnames()}
             if len(root) != 1:
                 raise ValueError(
-                    f"Expected a single top-directory in {os.fspath(archive_path)!r}, "
-                    f"observed {len(root)}"
+                    f"Expected a single top-directory in {archive_path!r}, observed {len(root)}"
                 )
             output_dir = root.pop()
-            logger.info(f"Unpack archive {os.fspath(archive_path)!r} to {output_dir!r}")
+            logger.info(f"Unpack archive {archive_path!r} to {output_dir!r}")
             f2.extractall()
     finally:
         if delete_archive and os.path.isfile(archive_path):
@@ -59,7 +87,18 @@ def configure(
     build_path: str | os.PathLike[str] = "build",
     config_args: Iterable[str] = (),
 ) -> None:
-    """Run the ``configure`` executable from the passed source path."""
+    """Run the ``configure`` executable from the passed source path.
+
+    Parameters
+    ----------
+    src_path : str | os.PathLike[str]
+        The path to the source directory.
+    build_path : str | os.PathLike[str]
+        The path to the to-be created build directory.
+    config_args : Iterable[str]
+        Arguments for the ``configure`` executable in ``src_path``.
+
+    """
     config_path = os.path.join(src_path, "configure")
     os.chmod(config_path, stat.S_IRUSR | stat.S_IXUSR)
 
@@ -74,7 +113,16 @@ def read_config_log(
     build_path: str | os.PathLike[str] = "build",
     log_name: str | os.PathLike[str] = "config.log",
 ) -> None:
-    """Write the ``./configure`` output to the logger."""
+    """Write the ``./configure`` output to the logger.
+
+    Parameters
+    ----------
+    build_path : str | os.PathLike[str]
+        The path to the build directory.
+    log_name : str | os.PathLike[str]
+        The name of the logfile inside ``build_path``.
+
+    """
     log_file = os.path.join(build_path, log_name)
     if not os.path.isfile(log_file):
         logger.debug(f"No such file: {log_file!r}")
@@ -86,6 +134,17 @@ def read_config_log(
 
 
 def build(build_path: str | os.PathLike[str], cpu_count: int | None = None) -> None:
+    """Build and install a package via ``make``.
+
+    Parameters
+    ----------
+    build_path : str | os.PathLike[str]
+        The path to the build directory.
+    cpu_count : int | None
+        The number of CPU cores to use for the build process.
+        Using :data:`None` defaults to the output of :func:`os.cpu_count`.
+
+    """
     if cpu_count is None:
         cpu_count = os.cpu_count()
     else:
