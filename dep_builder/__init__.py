243f861fcf0a9185d26eb2b41c90a7289c9371c7
Bas van Beek <bas.vanbeek@hotmail.com>
2023-08-02 15:37:31 +0200

    BLD: Switch from setup.py to pyproject.toml

--- dep_builder/__init__.py
+++ dep_builder/__init__.py
@@ -13,6 +13,7 @@ Index
     TimeLogger
     logger
     __version__
+    __version_tuple__
 
 API
 ---
@@ -26,6 +27,7 @@ API
 .. autoclass:: TimeLogger
 .. autodata:: logger
 .. autodata:: __version__
+.. autodata:: __version_tuple__
 
 """
 
@@ -33,12 +35,13 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from ._version import __version__
+from ._version import __version__, __version_tuple__
 from ._logger import logger, TimeLogger, BaseTimeLogger
 from ._core import download_and_unpack, configure, read_config_log, build, parse_version
 
 __all__ = [
     "__version__",
+    "__version_tuple__",
     "logger",
     "BaseTimeLogger",
     "TimeLogger",
@@ -54,6 +57,9 @@ __all__ = [
 if not TYPE_CHECKING:
     #: The :mod:`dep_builder` version.
     __version__: str
+
+    #: The :mod:`dep_builder` version as a tuple.
+    __version_tuple__: tuple
 
     #: The :mod:`dep_builder` logger.
     logger: logging.Logger
